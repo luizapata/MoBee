dd9b19e63285c6f70cc29d67a6f90ac090f8a3af
luisa <Luisa@Luisas-MacBook-Pro.local>
2016-08-11 13:45:10 -0700

    server is working now

---
+++ mobee-app/server.js
@@ -0,0 +1,17 @@
+var express  = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();                  
+
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json()); 
+
+
+var port = process.env.PORT || 8080;
+app.set('port', port);
+
+
+app.listen(port, function () {
+  console.log('listening on port ' + port);
+});
